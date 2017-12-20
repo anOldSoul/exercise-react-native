65ad755f2bb7622a0fbc8a1360b2e3a2ee36d6ab
wf <wufang@carelinker.com>
2017-12-20 19:38:33 +0800

    健康档案

--- newProject/App.js
+++ newProject/App.js
@@ -25,16 +25,30 @@ export default class App extends Component<{}> {
       <View style={styles.container}>
         <View style={styles.head}>
           <Image style={styles.headImg} source={require('./images/head.png')} />
+          <Text style={styles.headName}>Huiling</Text>
+          <Text style={styles.title}>为定制专属您的健康管理，请完善信息</Text>
+          <Text style={styles.progress}></Text>
         </View>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={styles.instructions}>
+          <Text>基本信息</Text>
+          <Image style={styles.arrowImg} source={require('./images/ic_up.png')} />
+        </View>
+        <View style={styles.instructions}>
+          <Text>健康信息</Text>
+          <Image style={styles.arrowImg} source={require('./images/ic_up.png')} />
+        </View>
+        <View style={styles.instructions}>
+          <Text>用药信息</Text>
+          <Image style={styles.arrowImg} source={require('./images/ic_up.png')} />
+        </View>
+        <View style={styles.instructions}>
+          <Text>体检信息</Text>
+          <Image style={styles.arrowImg} source={require('./images/ic_up.png')} />
+        </View>
+        <View style={styles.instructions}>
+          <Text>健康报告</Text>
+          <Image style={styles.arrowImg} source={require('./images/ic_up.png')} />
+        </View>
       </View>
     );
   }
@@ -50,12 +64,31 @@ const styles = StyleSheet.create({
   head: {
     height: 205,
     alignItems: 'center',
-    backgroundColor: '#1989fa'
+    backgroundColor: '#2ac4fb'
   },
   headImg: {
     width: 65,
     height: 65,
-    borderRadius: 32.5
+    borderRadius: 32.5,
+    marginTop: 20
+  },
+  headName: {
+    color: '#fff',
+    marginTop: 5,
+    opacity: 0.5,
+    fontSize: 10
+  },
+  title: {
+    color: '#fff',
+    marginTop: 5,
+    fontSize: 10
+  },
+  progress: {
+    width: 130,
+    height: 10,
+    backgroundColor: '#fff',
+    marginTop: 10,
+    borderRadius: 1
   },
   welcome: {
     fontSize: 20,
@@ -63,8 +96,13 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    height: 45,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderBottomColor: '#d2d3d5',
+    borderBottomWidth: 0.5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  }
 });
